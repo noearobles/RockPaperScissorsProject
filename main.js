6ff569e71f424013b44e223b63518075230d4fdb
noearobles <roblesnoe1030@gmail.com>
2022-12-04 22:09:59 -0600

    modified js

--- main.js
+++ main.js
@@ -1,62 +1,61 @@
 // uses strict mode so strings are not coerced, variables are not hoisted, etc... 
 'use strict';
 
-// brings in the assert module for unit testing
-const assert = require('assert');
 // brings in the readline module to access the command line
 const readline = require('readline');
 // use the readline module to print out to the command line
-const rl = readline.createInterface({
+const rpsInterface = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
 
+console.log("Welcome to Rock Paper Scissors!")
+
 // the function that will be called by the unit test below
-const rockPaperScissors = (hand1, hand2) => {
 
-  // Write code here
-  // Use the unit test to see what is expected
+function logic(userChoice, computerChoice) {
+  if (userChoice === computerChoice) {
+    console.log('This was a draw');
+    console.log("If you want to settle this, rerun 'node main.js'");
+    return;
+  }
+  else if ((userChoice === "rock" && computerChoice === "scissors") || (userChoice === "paper" && computerChoice === "rock") || (userChoice === "scissors" && computerChoice === "paper")
+  ) {
+    console.log('Congratulations, you won!');
+    console.log("Wanna play again? Rerun 'node main.js'");
 
-}
+    return;
+  } else {
+    console.log('Nice try but I WIN sucker');
+    console.log("Wanna give another shot? Rerun 'node main.js'");
+    return;
+  }
+}; 
 
 // the first function called in the program to get an input from the user
 // to run the function use the command: node main.js
-// to close it ctrl + C
-function getPrompt() {
-  rl.question('hand1: ', (answer1) => {
-    rl.question('hand2: ', (answer2) => {
-      console.log( rockPaperScissors(answer1, answer2) );
-      getPrompt();
-    });
-  });
-}
+// to close it ctrl + Cre
 
-// Unit Tests
-// to use them run the command: npm test main.js
-// to close them ctrl + C
-if (typeof describe === 'function') {
+rpsInterface.question('Pick a hand: ', (userInput) => {
+  const userChoice = userInput.toLowerCase();
+  if (userChoice !== "rock" && userChoice !== "paper" && userChoice !== "scissors") {
+    console.log("invalid input provided, has to be rock, paper, or scissors");
+    console.log("Rerun 'node main.js' and try again");
+    rpsInterface.close();
+    return;
+  }
 
-  // most are notes for human eyes to read, but essentially passes in inputs then compares if the function you built returns the expected output.
-  describe('#rockPaperScissors()', () => {
-    it('should detect a tie', () => {
-      assert.equal(rockPaperScissors('rock', 'rock'), "It's a tie!");
-      assert.equal(rockPaperScissors('paper', 'paper'), "It's a tie!");
-      assert.equal(rockPaperScissors('scissors', 'scissors'), "It's a tie!");
-    });
-    it('should detect which hand won', () => {
-      assert.equal(rockPaperScissors('rock', 'paper'), "Hand two wins!");
-      assert.equal(rockPaperScissors('paper', 'scissors'), "Hand two wins!");
-      assert.equal(rockPaperScissors('rock', 'scissors'), "Hand one wins!");
-    });
-    it('should scrub input to ensure lowercase with "trim"ed whitepace', () => {
-      assert.equal(rockPaperScissors('rOcK', ' paper '), "Hand two wins!");
-      assert.equal(rockPaperScissors('Paper', 'SCISSORS'), "Hand two wins!");
-      assert.equal(rockPaperScissors('rock ', 'sCiSsOrs'), "Hand one wins!");
-    });
-  });
-} else {
+  const possibleChoices = ["rock", "paper", "scissors"];
 
-  // always returns ask the user for another input
-  getPrompt();
+  const computerChoice = possibleChoices[Math.floor(Math.random() * possibleChoices.length)];
 
-}
+
+  console.log(`You picked: ${userChoice}`);
+  console.log(`I picked: ${computerChoice}`);
+
+  logic(userChoice, computerChoice);
+
+  rpsInterface.close();
+
+
+});
